fe37c3f48184cb91b7f9b849c6471e6b3d6e34b3
archit-roy <architroy0@gmail.com>
2026-02-03 22:42:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const toggle = document.getElementById("themeToggle");
-const body = document.body;
-
-// default dark
-toggle.checked = false;
 
 toggle.addEventListener("change", () => {
-  body.setAttribute(
-    "data-theme",
-    toggle.checked ? "light" : "dark"
-  );
+  document.body.classList.toggle("dark", toggle.checked);
 });
